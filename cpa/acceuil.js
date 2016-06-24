d3d7814c0cdfcc4dc0db3bf3089a0bda650c6ebb
Hugo MAROT <hmarot@e1r1p12.42.fr>
2016-06-24 19:43:51 +0200

    lapin malin

---
+++ cpa/acceuil.js
@@ -0,0 +1,5 @@
+var tab_test = []
+tab_test[0] = new creat_css("ressource/cv_blue.png");
+tab_test[1] = new creat_css("ressource/cv_blue.png");
+tab_test[2] = new creat_css("ressource/balance_o.png");
+
--- cpa/lib_js.js
+++ cpa/lib_js.js
@@ -1,9 +1,28 @@
 function	creat_div_into(d_css, parent, content, id)
 {
 	if (arguments.length == 4)
-		$('div id = '+id+' class = '+arguments[3]+' >').html(content).appento(parent);
+		$('div id = '+id+' class = '+arguments[3]+' >').html(content).appento($(parent));
 	else 
-		$('div id = '+id+' >').html(content).appento(parent);
+		$('div id = '+id+' >').html(content).appento(parent)
 	d_css.css_aply(id);
 }
 
+function	creat_css(img, d_start, d_end, color)
+{
+	this.logo_src = img;
+	this.start_date = d_start;
+	this.end_date = d_end;
+	this.color = color;
+	this.css_aply = function(id, today)
+	{
+		$('#' + id + 'img src = ' + img).appento($('#' + id));
+		$('#' + id).css('left', ft_comp_date(today, d_start));
+		$('#' + id).css('width', ft_comp_date(d_start, d_end));
+		$('#' + id).css('background-color', color);
+	};
+}
+
+function	ft_comp_date(date_1, date_2)
+{
+	return ((date_2 - date_1) / (3600 * 24));
+}
