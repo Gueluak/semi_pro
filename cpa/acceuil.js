00c7fd1af77fc23fae7fc57327641d51d7e225ad
Hugo MAROT <hmarot@e1r1p12.42.fr>
2016-06-29 19:13:49 +0200

    lapinoux

--- cpa/acceuil.js
+++ cpa/acceuil.js
@@ -3,3 +3,10 @@ tab_test[0] = new creat_css("ressource/cv_blue.png");
 tab_test[1] = new creat_css("ressource/cv_blue.png");
 tab_test[2] = new creat_css("ressource/balance_o.png");
 
+function resize(id, size)
+{
+	if ($(id).css('height') == "0px")
+		$(id).css('height', size);
+	else
+		$(id).css('height', '0px');
+}
